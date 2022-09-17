0fc6674ebc8128b1a361802bac9cd685b0c88e3e
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-09-17 20:34:44 +0100

    i added button

---
+++ script.js
@@ -0,0 +1,65 @@
+
+function add(){
+    for (var i = Number(digitInput.value); i<13; i++){
+       for (var j = 1; j<13; j++){
+           myTable.innerHTML += "<tr><td>" + i + "+" + j + "=" + (i+j) + "</tr></td>"
+       }
+    }   
+   }
+   
+   function subtract(){
+       for (var i = Number(digitInput.value); i<13; i++){
+          for (var j = 1; j<13; j++){
+              myTable.innerHTML += "<tr><td>" + i + "-" + j + "=" + (i-j) + "</tr></td>"
+          }
+       }   
+      }
+   
+      function multiply(){
+       for (var i = Number(digitInput.value); i<13; i++){
+          for (var j = 1; j<13; j++){
+              myTable.innerHTML += "<tr><td>" + i + "*" + j + "=" + (i*j) + "</tr></td>"
+          }
+       }   
+      }
+      
+      function divide(){
+       for (var i = Number(digitInput.value); i<13; i++){
+          for (var j = 1; j<13; j++){
+              myTable.innerHTML += "<tr><td>" + i + "%" + j + "=" + (i%j) + "</tr></td>"
+          }
+       }   
+      }
+   
+// function add(){
+//     for (var i = Number(digitInput.value); i<13; i++){
+//        for (var j = 1; j<13; j++){
+//            myTable.innerHTML += "<tr><td>" + i + "+" + j + "=" + (i+j) + "</tr></td>"
+//        }
+//     }   
+//    }
+   
+//    function subtract(){
+//        for (var i = Number(digitInput.value); i<13; i++){
+//           for (var j = 1; j<13; j++){
+//               myTable.innerHTML += "<tr><td>" + i + "-" + j + "=" + (i-j) + "</tr></td>"
+//           }
+//        }   
+//       }
+   
+//       function multiply(){
+//        for (var i = Number(digitInput.value); i<13; i++){
+//           for (var j = 1; j<13; j++){
+//               myTable.innerHTML += "<tr><td>" + i + "*" + j + "=" + (i*j) + "</tr></td>"
+//           }
+//        }   
+//       }
+      
+//       function divide(){
+//        for (var i = Number(digitInput.value); i<13; i++){
+//           for (var j = 1; j<13; j++){
+//               myTable.innerHTML += "<tr><td>" + i + "%" + j + "=" + (i%j) + "</tr></td>"
+//           }
+//        }   
+//       }
+         
